bc798fd3198bdbd32cff606da365f75f60931c36
jmcfx <uispoj@gmail.com>
2024-04-22 18:29:22 +0100

    added imagePicker

--- lib/features/community/screens/edit_community_screen.dart
+++ lib/features/community/screens/edit_community_screen.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:reddit_app/core/common/error_text.dart';
+import 'package:reddit_app/core/common/loader.dart';
+import 'package:reddit_app/core/constants/constants.dart';
+import 'package:reddit_app/core/utils.dart';
+import 'package:reddit_app/features/auth/controller/community_controller.dart';
 import 'package:reddit_app/theme/palette.dart';
 
 class EditCommunityScreen extends ConsumerStatefulWidget {
@@ -12,20 +21,112 @@ class EditCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
+  File? bannerFile, profileFile;
+  void selectBannerImage() async {
+    final res = await pickImage();
+
+    if (res != null) {
+      setState(() {
+        bannerFile = File(res.files.first.path!);
+      });
+    }
+  }
+
+  void selectProfileImage() async {
+    final res = await pickImage();
+
+    if (res != null) {
+      setState(() {
+        profileFile = File(res.files.first.path!);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Palette.darkModeAppTheme.colorScheme.background,
-      appBar: AppBar(
-        title: const Text('Edit Community'),
-        centerTitle: false,
-        actions: [
-          TextButton(onPressed: (){}, child: const Text('Save'))
-        ],
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+    return ref.watch(getCommunityByNameProvider(widget.name)).when(
+          data: (community) => Scaffold(
+            backgroundColor: Palette.darkModeAppTheme.colorScheme.background,
+            appBar: AppBar(
+              title: const Text('Edit Community'),
+              centerTitle: false,
+              actions: [
+                TextButton(onPressed: () {}, child: const Text('Save'))
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0).r,
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        child: Stack(
+                          children: [
+                            GestureDetector(
+                              onTap: selectBannerImage,
+                              child: DottedBorder(
+                                borderType: BorderType.RRect,
+                                radius: const Radius.circular(10).r,
+                                dashPattern: const [10, 4],
+                                strokeCap: StrokeCap.round,
+                                color: Palette.darkModeAppTheme.textTheme
+                                    .bodyMedium!.color!,
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 150.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10).r,
+                                  ),
+                                  child: bannerFile != null
+                                      ? Image.file(bannerFile!)
+                                      : community.banner.isEmpty ||
+                                              community.banner ==
+                                                  Constants.bannerDefault
+                                          ? const Center(
+                                              child: Icon(
+                                                Icons.camera_alt_outlined,
+                                                size: 40,
+                                              ),
+                                            )
+                                          : Image.network(community.banner),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0).r,
+                                child: GestureDetector(
+                                  onTap: selectProfileImage,
+                                  child:  profileFile!= null ? CircleAvatar(
+                                    backgroundImage:  FileImage(
+                                      profileFile!
+                                    ),
+                                    radius: 32.r,
+                                  ) : CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                            community.avatar,
+                                          ),
+                                          radius: 32.r,
+                                        ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          error: ((error, stackTrace) => ErrorText(
+                error: error.toString(),
+              )),
+          loading: () => const Loader(),
+        );
   }
 }
